6392a01270e4f447ac7fcaa4640ac91195664a74
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-04-26 17:09:48 -0300

    padrão para module exports

--- src/contador.js
+++ src/contador.js
@@ -49,15 +49,8 @@ App.Contador = (function() {
     return Contador;
 }());
 
-(function(root, factory) {
+(function(factory) {
     if (typeof module === 'object' && module.exports) {
-        // Node/CommonJS
         module.exports = factory;
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(factory);
-    } else {
-        // Browser globals
-        root.ModuleName = factory;
     }
-})(this, App);
+})(App);
